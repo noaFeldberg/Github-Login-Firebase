8284038b8c73d67b6d62e58c1564e464c5e627e0
noa_feldberg <noa@clarifruit.com>
2020-11-05 15:39:37 +0200

    Add auth and github services

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails(userName: string) {
+    return this.http.get(`http://api.github.com/users/${userName}`);
+  }
+
+  getRepos(repoUrl: string) {
+    return this.http.get(repoUrl);
+  }
+
 }
