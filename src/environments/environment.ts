213c6b9a86ac73318d175b528ac0c50508c0cc95
noa_feldberg <noa@clarifruit.com>
2020-11-05 15:20:10 +0200

    Initial data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyDLqkYqu68QryfLw8hiKbdSk5BzYsH54ng",
+    authDomain: "git-project-firebase.firebaseapp.com",
+    databaseURL: "https://git-project-firebase.firebaseio.com",
+    projectId: "git-project-firebase",
+    storageBucket: "git-project-firebase.appspot.com",
+    messagingSenderId: "73583598880",
+    appId: "1:73583598880:web:d249128bd4df046417cb45",
+    measurementId: "G-8283BFSQFT"
+    }
 };
 
 /*
